d990d30a060b36fd73bc1d33eef95af8feaf08ab
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-12-14 15:19:32 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,20 +4,32 @@ class RidesController < ApplicationController
      @attraction = Attraction.find(params[:attraction_id])
      ride_the_ride if valid_ride?
      @user = current_user
-    # binding.pry
      redirect_to user_path(current_user)
    end
 
    private
     def valid_ride?
-      #if height and tickets is greater than or equal  attraction and tickets
-      @attraction.min_height <= current_user.height && @attraction.tickets <= current_user.tickets
+      if @attraction.min_height > current_user.height && @attraction.tickets > current_user.tickets
+        flash[:message] = "You are not tall enough to ride the #{@attraction.name} You do not have enough tickets to ride the #{@attraction.name}"
+        false
+      elsif @attraction.tickets > current_user.tickets
+        flash[:message] = "You do not have enough tickets to ride the #{@attraction.name}"
+        false
+      elsif @attraction.min_height > current_user.height
+        flash[:message] = "You are not tall enough to ride the #{@attraction.name}"
+        false
+      else
+        true
+      end
     end
 
     def ride_the_ride
       user = current_user
+      user.nausea += @attraction.nausea_rating
+      user.happiness += @attraction.happiness_rating
       user.tickets -= @attraction.tickets
       user.save
+      flash[:message] = "Thanks for riding the #{@attraction.name}!"
     #  binding.pry
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#welcome"
   post "/rides", to: "rides#create"
   resources :users
-  resources :attractions, only: [:index, :show]
+  resources :attractions, except: [:destroy]
   #resources :rides, only: [:create]
 
   get "/signin", to: "users#signin"
